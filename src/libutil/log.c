431681bae0849b49a99341f036fc9742d01403df
zpcoding <intraceing@outlook.com>
2021-03-16 14:52:27 +0800

    update

--- src/libutil/log.c
+++ src/libutil/log.c
@@ -37,7 +37,7 @@ void good_log_init(const char *ident, int copy2stderr)
 {
     static good_log_t log = {LOG_UPTO(LOG_INFO),NULL,NULL};
 
-    assert(__good_log_ctx = NULL);
+    assert(__good_log_ctx == NULL);
 
     openlog(ident,LOG_CONS | LOG_PID | (copy2stderr ? LOG_PERROR : 0), LOG_USER);
     setlogmask(log.mask);
@@ -48,10 +48,45 @@ void good_log_init(const char *ident, int copy2stderr)
     __good_log_ctx = &log;
 }
 
+void good_log_redirect(void (*agent_cb)(void *opaque,int level,const char* fmt,va_list args),void *opaque)
+{
+    if(!__good_log_ctx)
+        return;
+    
+    assert(agent_cb);
+
+    __good_log_ctx->agent_cb = agent_cb;
+    __good_log_ctx->opaque = opaque;
+}
+
 int good_log_mask(int mask)
 {
     if(!__good_log_ctx)
         return 0;
     
     return setlogmask(__good_log_ctx->mask = mask);
+}
+
+void good_log_printf(int level,const char* fmt,...)
+{
+    va_list args;
+    va_start(args, fmt);
+    good_log_vprintf(level,fmt, args);
+    va_end(args);
+}
+
+void good_log_vprintf(int level,const char* fmt,va_list args)
+{
+    if(!__good_log_ctx)
+        return;
+
+    if(__good_log_ctx->agent_cb)    
+    {
+        if(LOG_MASK(level) & __good_log_ctx->mask)
+            __good_log_ctx->agent_cb(__good_log_ctx->opaque,level,fmt,args);
+    }
+    else
+    {
+        vsyslog(level,fmt,args);
+    } 
 }
--- src/libutil/log.h
+++ src/libutil/log.h
@@ -14,15 +14,25 @@
 #include <stdarg.h>
 
 #include "general.h"
-#include "thread.h"
 
 /**
  * @brief 初始化日志环境
  * 
- * @param ident 自定义标识。
- * @param copy2stderr !0 复制输出到stderr。
+ * @param ident NULL(0) 进程名做为标识，!NULL(0) 自定义标识。
+ * @param copy2stderr 0 仅记录，!0 复制到stderr。
+ * 
+ * @see openlog()
+ * @see setlogmask()
  */
 void good_log_init(const char *ident, int copy2stderr);
+
+/**
+ * @brief 日志重定向
+ * 
+ * @param agent_cb 
+ * @param opaque 
+ */
+void good_log_redirect(void (*agent_cb)(void *opaque,int level,const char* fmt,va_list args),void *opaque);
 
 /**
  * @brief 设置日志掩码
@@ -30,11 +40,31 @@ void good_log_init(const char *ident, int copy2stderr);
  * @param mask 新的掩码
  * 
  * @return 旧的掩码
+ * 
+ * @see setlogmask()
  */
 int good_log_mask(int mask);
 
-
+/**
+ * @brief 记录日志
+ * 
+ * @param level LOG_* in syslog.h
+ * @param fmt 
+ * @param ... 
+ * 
+ * @see good_log_vprintf()
+ */
 void good_log_printf(int level,const char* fmt,...);
 
+/**
+ * @brief 记录日志
+ * 
+ * @param level LOG_* in syslog.h
+ * @param fmt 
+ * @param args 
+ * 
+ * @see vsyslog()
+ */
+void good_log_vprintf(int level,const char* fmt,va_list args);
 
 #endif //GOOD_UTIL_LOG_H
