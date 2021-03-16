c670a0a32223e5d60a7ce55c7606810fec5f47f8
zpcoding <intraceing@outlook.com>
2021-03-16 14:52:43 +0800

    update

---
+++ src/tests/log_test.c
@@ -0,0 +1,32 @@
+/*
+ * This file is part of GOOD.
+ * 
+ * MIT License
+ * 
+ */
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+#include "libutil/log.h"
+
+void agent(void *opaque,int level,const char* fmt,va_list args)
+{
+    fprintf(stderr,"LV[%d]: ",level);
+    vfprintf(stderr,fmt,args);
+}
+
+int main(int argc, char **argv)
+{
+
+    good_log_init(NULL,1);
+
+  //  good_log_redirect(agent,NULL);
+
+    good_log_mask(LOG_UPTO(LOG_DEBUG));
+
+    for(int i = LOG_EMERG ;i<= LOG_DEBUG;i++)
+        good_log_printf(i,"haha-%d",i);
+
+    return 0;
+}
