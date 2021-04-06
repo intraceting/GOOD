1677c044a7d96c17d324fc6a66a30a8976c223c6
zpcoding <intraceing@outlook.com>
2021-04-06 16:04:21 +0800

    update

--- src/tests/geometry_test.c
+++ src/tests/geometry_test.c
@@ -10,7 +10,7 @@
 #include <string.h>
 #include "libutil/geometry.h"
 
-#define PI (3.1415926)
+
 
 void test1()
 {
@@ -24,7 +24,7 @@ void test1()
     printf("d=%lf\n",d);
 
     double r = good_geom_halfline_radian(&p1,&p2,'X');
-    double a = r*180/PI;
+    double a = r*180/M_PIl;
     printf("r=%lf;a=%lf\n",r,a);
 
     good_point_t p3 = {0,0,0};
@@ -52,18 +52,48 @@ void test2()
     double d2 = good_geom_p2p_distance(&p,&p2);
     double r1 = good_geom_halfline_radian(&p,&p1,'X');
     double r2 = good_geom_halfline_radian(&p,&p2,'X');
-    double R1 = r1*180/PI;
-    double R2 = r2*180/PI;
+    double R1 = r1*180/M_PIl;
+    double R2 = r2*180/M_PIl;
 
     printf("d1(%lf)-d2(%lf)=%lf\n",d1,d2,d1-d2);
     printf("r1(%lf)-r2(%lf)=%lf\n",r1,r2,r1-r2);
     printf("R1(%lf)-R2(%lf)=%lf\n",R1,R2,R1-R2);
 }
 
+void GuestLocation(const good_point_t *p1,const good_point_t *p2,double xspeed,good_point_t *p3)
+{
+    double d = good_geom_p2p_distance(p1,p2);
+    double r = good_geom_halfline_radian(p1,p2,'X');
+    good_geom_point_move(p2,r,d*xspeed,p3);
+}
+
+void test3()
+{
+    good_point_t p1 = {45,45,0};
+    good_point_t p2 = {45,45,0};
+    good_point_t p3 = {0,0,0};
+
+    GuestLocation(&p1,&p2,1,&p3);
+    printf("p1={%lf,%lf},p2={%lf,%lf},p3={%lf,%lf}\n",p1.x,p1.y,p2.x,p2.y,p3.x,p3.y);
+
+    GuestLocation(&p1,&p2,2,&p3);
+    printf("p1={%lf,%lf},p2={%lf,%lf},p3={%lf,%lf}\n",p1.x,p1.y,p2.x,p2.y,p3.x,p3.y);
+
+    GuestLocation(&p1,&p2,3,&p3);
+    printf("p1={%lf,%lf},p2={%lf,%lf},p3={%lf,%lf}\n",p1.x,p1.y,p2.x,p2.y,p3.x,p3.y);
+
+    GuestLocation(&p1,&p2,0.1,&p3);
+    printf("p1={%lf,%lf},p2={%lf,%lf},p3={%lf,%lf}\n",p1.x,p1.y,p2.x,p2.y,p3.x,p3.y);
+
+
+}
+
 int main(int argc, char **argv)
 {
     //test1();
-    test2();
+    //test2();
+
+    test3();
 
     return 0;
 }
