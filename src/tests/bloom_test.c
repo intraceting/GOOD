175a142bfdf42717f38c2773579db31432ad7846
zpcoding <intraceing@outlook.com>
2021-04-08 10:28:40 +0800

    update

--- src/tests/bloom_test.c
+++ src/tests/bloom_test.c
@@ -9,9 +9,9 @@
 #include <unistd.h>
 #include <string.h>
 #include "libutil/general.h"
+#include "libutil/base64.h"
 
-
-int main(int argc, char **argv)
+void test_bloom()
 {
     size_t s = 10000000000;
     uint8_t* buf = (uint8_t*)good_heap_alloc(s);
@@ -41,7 +41,38 @@ int main(int argc, char **argv)
 
 
     good_heap_free(buf);
+}
 
+void test_base64()
+{
+    //char src[] = {"a"};
+   // char src[] = {"ab"};
+    //char src[] = {"abc"};
+    //char src[] = {"abcdefg1234567"};
+    char src[] = {"abcdefghigklmnopqrstuvwxyz"};
+    uint8_t dst[100] = {0};
+    char src2[100] = {0};
+
+    ssize_t dlen = good_base64_encode(src,strlen(src),NULL,0);
+
+    ssize_t d2len = good_base64_encode(src,strlen(src),dst,dlen);
+
+    printf("dst(%ld)={%s}\n",d2len,dst);
+
+    ssize_t slen = good_base64_decode(dst,d2len,NULL,0);
+
+    ssize_t s2len = good_base64_decode(dst,d2len,src2,slen);
+
+    printf("src2(%ld)={%s}\n",s2len,src2);
+
+}
+
+
+int main(int argc, char **argv)
+{
+    //test_bloom();
+
+    test_base64();
 
     return 0;
 }
