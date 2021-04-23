60b6139427f077264e29f993378d7a024c28e061
zpcoding <intraceing@outlook.com>
2021-04-23 16:21:52 +0800

    update

--- src/libutil/tar.c
+++ src/libutil/tar.c
@@ -6,6 +6,154 @@
  */
 #include "tar.h"
 
+/*
+ * 以下几个方法的来源：http://www.ncbi.nlm.nih.gov/IEB/ToolBox/CPP_DOC/lxr/source/src/util/compress/api/tar.cpp 
+*/
+
+/** 
+ * Convert a number to an octal string padded to the left
+ * with [leading] zeros ('0') and having _no_ trailing '\0'.
+*/
+static int NumToOctal(uint64_t val, char *ptr, size_t len)
+{
+    assert(len > 0);
+
+    do
+    {
+        ptr[--len] = '0' + (char)(val & 7);
+        val >>= 3;
+    } while (len);
+
+    return val ? 0 : 1;
+}
+
+/** 
+ * Convert an octal number (possibly preceded by spaces) to numeric form.
+ *  Stop either at the end of the field or at first '\0' (if any).
+*/
+static int OctalToNum(uint64_t* val, const char* ptr, size_t len)
+{
+    assert(len > 0);
+
+    size_t i = *ptr ? 0 : 1;
+    while (i < len && ptr[i])
+    {
+        if (!isspace((unsigned char) ptr[i]))
+            break;
+        i++;
+    }
+    *val = 0;
+    int okay = 0;
+    while (i < len && ptr[i] >= '0' && ptr[i] <= '7')
+    {
+        okay = 1;
+        *val <<= 3;
+        *val |= ptr[i++] - '0';
+    }
+    while (i < len && ptr[i])
+    {
+        if (!isspace((unsigned char) ptr[i]))
+            return 0;
+        i++;
+    }
+
+    return okay;
+}
+
+/**
+ *
+*/
+static int NumToBase256(uint64_t val, char* ptr, size_t len)
+{
+    assert(len > 0);
+
+    do
+    {
+        ptr[--len] = (unsigned char) (val & 0xFF);
+        val >>= 8;
+    }
+    while (len);
+
+    *ptr |= '\x80'; // set base-256 encoding flag
+
+    return val ? 0 : 1;
+}
+
+/**
+ * Return 0 (false) if conversion failed; 1 if the value converted to
+ * conventional octal representation (perhaps, with terminating '\0'
+ * sacrificed), or -1 if the value converted using base-256.
+*/
+static int EncodeUint8(uint64_t val, char* ptr, size_t len)
+{
+    // Max file size (for len == 12):
+    if (NumToOctal(val, ptr, len))
+    {
+        //   8GiB-1         
+        return 1/*okay*/;
+    }
+    if (NumToOctal(val, ptr, ++len))
+    {
+        //   64GiB-1
+        return 1/*okay*/;
+    }
+    if (NumToBase256(val, ptr, len))
+    {
+        //   up to 2^94-1
+        return -1/*okay, base-256*/;
+    }
+    return 0/*failure*/;
+}
+
+/**
+ * Return true if conversion succeeded;  false otherwise.
+*/ 
+static int Base256ToNum(uint64_t* val, const char* ptr, size_t len)
+{
+    const uint64_t lim = UINT64_MAX >> 8;
+
+    if (*ptr & '\x40')
+    { 
+        // negative base-256?
+        return 0;
+    }
+
+    *val = *ptr++ & '\x3F';
+
+    while (--len)
+    {
+        if (*val > lim)
+        {
+            return 0;
+        }
+        *val <<= 8;
+        *val |= (unsigned char) (*ptr++);
+    }
+
+    return 1;
+}
+
+/**
+ * Return 0 (false) if conversion failed; 1 if the value was read into
+ * as a conventional octal string (perhaps, without the terminating '\0');
+ * or -1 if base-256 representation used.
+*/
+static int DecodeUint8(uint64_t* val, const char* ptr, size_t len)
+{
+    if (*ptr & '\x80')
+    {
+        return Base256ToNum(val, ptr, len) ? -1/*okay*/ : 0/*failure*/;
+    }
+    else
+    {
+        return OctalToNum(val, ptr, len) ? 1/*okay*/ : 0/*failure*/;
+    }
+}
+
+/*
+ * 以上几个方法的来源：http://www.ncbi.nlm.nih.gov/IEB/ToolBox/CPP_DOC/lxr/source/src/util/compress/api/tar.cpp 
+*/
+
 ssize_t good_tar_read(int fd, void *data, size_t size, good_buffer_t *buf)
 {
     ssize_t rsize = 0;
@@ -170,4 +318,83 @@ int good_tar_write_trailer(int fd, uint8_t stuffing, good_buffer_t *buf)
         return 0;
 
     return -1;
+}
+
+uint32_t good_tar_hdr_calc_checksum(good_tar_hdr *hdr)
+{
+    uint32_t sum = 0;
+    int i = 0;
+
+    assert(hdr != NULL);
+
+    for (i = 0; i < 148; ++i)
+    {
+        sum += *GOOD_PTR2PTR(uint8_t, hdr, i);
+    }
+
+    /*Skip CHECKSUM column(8 bytes)*/
+    for (i += 8; i < 512; ++i) //...........
+    {
+        sum += *GOOD_PTR2PTR(uint8_t, hdr, i);
+    }
+
+    sum += 256;
+
+    return sum;
+}
+
+uint32_t good_tar_hdr_get_checksum(good_tar_hdr* hdr)
+{
+    uint32_t sum = 0;
+    char buf[64] = {0};
+
+    assert(hdr != NULL);
+
+    memcpy(buf, hdr->chksum, sizeof (hdr->chksum));
+
+    sscanf(buf, "%o", &sum);
+
+    return sum;
+}
+
+int64_t good_tar_hdr_get_size(good_tar_hdr* hdr)
+{
+    uint32_t size = 0;
+    char buf[64] = {0};
+
+    assert(hdr != NULL);
+
+    memcpy(buf, hdr->size, sizeof (hdr->size));
+
+    if (DecodeUint8(&size, buf, sizeof(hdr->size)) == 0)
+        size = -1;
+
+    return size;
+}
+
+time_t good_tar_hdr_get_mtime(good_tar_hdr *hdr)
+{
+    time_t mtime = 0;
+    char buf[64] = {0};
+
+    assert(hdr != NULL);
+
+    memcpy(buf, hdr->mtime, sizeof (hdr->mtime));
+
+    sscanf(buf, "%lo", &mtime);
+
+    return mtime;
+}
+
+uint32_t good_tar_hdr_get_mode(good_tar_hdr *hdr)
+{
+    uint32_t mode = 0;
+    char buf[64] = {0};
+
+    assert(hdr != NULL);
+
+    memcpy(buf, hdr->mode, sizeof (hdr->mode));
+    sscanf(buf, "%o", &mode);
+
+    return mode;
 }
--- src/libutil/tar.h
+++ src/libutil/tar.h
@@ -11,11 +11,11 @@
 #include "buffer.h"
 
 /**
- * This is the ustar (Posix 1003.1) header.
+ * This is the tar header.
  * 
  * 512 Bytes.
  */
-typedef struct _good_ustar_hdr
+typedef struct _good_tar_hdr
 {
     char name[100];         /*   0 Dateiname*/
     char mode[8];           /* 100 Zugriffsrechte*/
@@ -33,8 +33,21 @@ typedef struct _good_ustar_hdr
     char devmajor[8];       /* 329 Major bei Geraeten*/
     char devminor[8];       /* 337 Minor bei Geraeten*/
     char prefix[155];       /* 345 Prefix fuer t_name*/
-    char mfill[12];         /* 500 Filler bis 512*/
-} good_ustar_hdr;
+
+    /*Other*/
+    union
+    {
+        /**
+         * This is the ustar (Posix 1003.1) header.
+        */
+        struct
+        {
+            char mfill[12]; /* 500 Filler bis 512*/
+        } ustar;
+
+    } other;
+
+} good_tar_hdr;
 
 /*
  * gnu tar extensions:
@@ -50,26 +63,57 @@ typedef struct _good_ustar_hdr
 #define GOOD_USTAR_LONGNAME_TYPE 'L'
 
 /**
- * TAR读。
+ * 从TAR文件读取数据。
+ * 
+ * @param buf NULL(0) 自由块大小，!NULL(0) 定长块大小。
  * 
  * @return > 0 读取的长度，<= 0 读取失败或已到末尾。
 */
 ssize_t good_tar_read(int fd, void *data, size_t size, good_buffer_t *buf);
 
 /**
- * TAR写。
+ * 向TAR文件写入数据。
+ * 
+ * @param buf NULL(0) 自由块大小，!NULL(0) 定长块大小。
  * 
  * @return > 0 写入的长度，<= 0 写入失败或空间不足。
 */
 ssize_t good_tar_write(int fd, const void *data, size_t size, good_buffer_t *buf);
 
 /**
- * TAR写末尾。
+ * 向TAR文件文件写入补齐数据。
+ * 
+ * @param buf NULL(0) 自由块大小，!NULL(0) 定长块大小。
  * 
  * @param stuffing 填充物。
  * 
  * @return > 0 缓存数据全部写入到文件，= 0 缓存无数据或无缓存，< 0 写入失败或空间不足(剩余数据在缓存中)。
 */
-int good_tar_write_trailer(int fd,uint8_t stuffing, good_buffer_t *buf);
+int good_tar_write_trailer(int fd, uint8_t stuffing, good_buffer_t *buf);
+
+/**
+ * 计算TAR头部校验和。
+*/
+uint32_t good_tar_hdr_calc_checksum(good_tar_hdr *hdr);
+
+/** 
+ * 提取TAR头部中的校验和字段。
+*/
+uint32_t good_tar_hdr_get_checksum(good_tar_hdr *hdr);
+
+/** 
+ * 提取TAR头部中的长度字段。
+*/
+int64_t good_tar_hdr_get_size(good_tar_hdr *hdr);
+
+/** 
+ * 提取TAR头部中的时间字段。
+*/
+time_t good_tar_hdr_get_mtime(good_tar_hdr *hdr);
+
+/** 
+ * 提取TAR头部中的状态字段。
+*/
+uint32_t good_tar_hdr_get_mode(good_tar_hdr *hdr);
 
 #endif //GOOD_UTIL_TAR_H
