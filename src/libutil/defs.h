f488f17063e72dc10176e24cd0cc5c8af75cc0e4
zpcoding <intraceing@outlook.com>
2021-04-29 09:03:35 +0800

    update

--- src/libutil/defs.h
+++ src/libutil/defs.h
@@ -68,84 +68,40 @@
  * @note 支持针地址偏移(Bytes)。
  * 
 */
-#define GOOD_PTR2PTR(T,PTR,OF)   ((T*)(((char*)(PTR))+(OF)))
+#define GOOD_PTR2PTR(T, PTR, OF) ((T *)(((char *)(PTR)) + (OF)))
 
 /**
  * 数值比较，返回最大值。
  * 
 */
-#define GOOD_MAX(A,B)   (((A)>(B))?(A):(B))
+#define GOOD_MAX(A, B) (((A) > (B)) ? (A) : (B))
 
 /**
  * 数值比较，返回最小值。
  * 
 */
-#define GOOD_MIN(A,B)   (((A)<(B))?(A):(B))
+#define GOOD_MIN(A, B) (((A) < (B)) ? (A) : (B))
 
 /**
  * 交换两个数值变量的值。
  * 
 */
-#define GOOD_INTEGER_SWAP(A,B)  ({(A)^=(B);(B)^=(A);(A)^=(B);})
+#define GOOD_INTEGER_SWAP(A, B) ({(A)^=(B);(B)^=(A);(A)^=(B); })
 
 /**
  * 设置出错码，并返回。
 */
-#define GOOD_ERRNO_AND_RETURN0(E) ({errno=(E);return;})
+#define GOOD_ERRNO_AND_RETURN0(E) ({errno=(E);return; })
 
 /**
  * 设置出错码，并返回值。
 */
-#define GOOD_ERRNO_AND_RETURN1(E, V) ({errno=(E);return (V);})
+#define GOOD_ERRNO_AND_RETURN1(E, V) ({errno=(E);return (V); })
 
 /**
  * 计算数组大小。
 */
-#define GOOD_ARRAY_SIZE(V)  (sizeof((V))/sizeof((V)[0]))
+#define GOOD_ARRAY_SIZE(V) (sizeof((V)) / sizeof((V)[0]))
 
-
-/*
- * 原子操作。
- *　
- * type __sync_fetch_and_add (type *ptr, type value);
- * type __sync_fetch_and_sub (type *ptr, type value);
- * type __sync_fetch_and_or (type *ptr, type value);
- * type __sync_fetch_and_and (type *ptr, type value);
- * type __sync_fetch_and_xor (type *ptr, type value);
- * type __sync_fetch_and_nand (type *ptr, type value);
- * type __sync_add_and_fetch (type *ptr, type value);
- * type __sync_sub_and_fetch (type *ptr, type value);
- * type __sync_or_and_fetch (type *ptr, type value);
- * type __sync_and_and_fetch (type *ptr, type value);
- * type __sync_xor_and_fetch (type *ptr, type value);
- * type __sync_nand_and_fetch (type *ptr, type value);
- * bool __sync_bool_compare_and_swap (type*ptr, type oldval, type newval, ...)
- * type __sync_val_compare_and_swap (type *ptr, type oldval,  type newval, ...)
-*/ 
-
-/**
- * 返回旧值。
-*/
-#define good_atomic_load(ptr)   __sync_and_and_fetch((ptr),*(ptr))
-
-/**
- * 设置新值。
-*/
-#define good_atomic_store(ptr,newval)   __sync_val_compare_and_swap((ptr),*(ptr),(newval))
-
-/**
- * 比较两个值。
-*/
-#define good_atomic_compare(ptr,oldval) __sync_bool_compare_and_swap((ptr),(oldval),*(ptr))
-
-/**
- * 比较两个值，相同则用新值替换旧值。
-*/
-#define good_atomic_compare_and_swap(ptr,oldval,newval) __sync_bool_compare_and_swap
-
-/**
- * 加法，返回旧值。
-*/
-#define good_atomic_fetch_and_add   __sync_fetch_and_add
 
 #endif //GOOD_UTIL_DEFS_H
